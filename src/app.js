e397e22b9e4701347c56f412ce9df91bacb638ef
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-01-15 00:33:30 +0000

    ejercicio de random card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const numeroCarta = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+  const figuraCarta = ['♦', '♥', '♠', '♣']
+
+  // crear un numero random del 1 al 13
+  const numeroRandom = Math.floor(Math.random() * 13)
+  const paloRandom = Math.floor(Math.random() * 4)
+
+  // mostrar el numero o figura segun el numeroRandom
+  function mostrarCarta () {
+    document.getElementById('palo-carta').innerHTML = figuraCarta[paloRandom]
+    document.getElementById('palo-carta2').innerHTML = figuraCarta[paloRandom]
+    document.getElementById('numero-carta').innerHTML = numeroCarta[numeroRandom]
+
+    if (figuraCarta[paloRandom] === '♠' || figuraCarta[paloRandom] === '♣'){
+      document.getElementById('palo-carta').style.color = "black"
+      document.getElementById('palo-carta2').style.color = "black"
+    }
+    
+
+  }
+
+  mostrarCarta()
+
 };
